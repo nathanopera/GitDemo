50eae6b59bd7b05b17957d4b383101f60da6ba2f
operanathan <nathanopera@gmail.com>
2022-02-17 12:27:47 -0800

    first commit

---
+++ src/main/java/qalinkacademy/mavenproject/seleniumautomation.java
@@ -0,0 +1,14 @@
+package qalinkacademy.mavenproject;
+
+import org.testng.annotations.Test;
+
+public class seleniumautomation {
+	
+	
+	@Test
+	public void testthis() {
+		
+	}
+			
+
+}
---
+++ src/test/java/qalinkacademy/mavenproject/SampleTest.java
@@ -0,0 +1,17 @@
+package qalinkacademy.mavenproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void display() {
+		assertTrue(true);
+		
+		System.out.println("hrllo");
+	}
+
+}
